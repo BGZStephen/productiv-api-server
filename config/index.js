db14e5853814a1de5a2d3cecba1950e9244d75a5
Stephen Wright <sjw948@gmail.com>
2017-07-10 21:38:59 +0100

    added jwt authentication to user routes

--- config/index.js
+++ config/index.js
@@ -4,12 +4,14 @@ const configs = {
   production: {
     database: 'mongodb://admin:yxe3tEcD7tA9@ds133981.mlab.com:33981/bgzstephen-productiv',
     environment: "production",
-    jwtSecret: "rdLsMzC5zhJj"
+    jwtSecret: "rdLsMzC5zhJj",
+    siteAuthToken: "NY58Avsh8CGe"
   },
   development: {
     database: 'mongodb://admin:yxe3tEcD7tA9@ds133981.mlab.com:33981/bgzstephen-productiv',
     environment: "development",
-    jwtSecret: "rdLsMzC5zhJj"
+    jwtSecret: "rdLsMzC5zhJj",
+    siteAuthToken: "NY58Avsh8CGe"
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -9,46 +9,58 @@ module.exports = router
 
 router.post("", (req, res, next) => {
 
-  let userObject = new User({
-    email: req.body.email,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    password: req.body.password
-  })
+  if(!req.query.siteAuthToken) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  } else if(req.query.siteAuthToken != Config.siteAuthToken) {
+    return res.status(403).json({error: "Unauthorized access, access denied"})
+  } else {
+    let userObject = new User({
+      email: req.body.email,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      password: req.body.password
+    })
 
-  User.create(userObject)
-  .then(user => {
-    res.sendStatus(200)
-  })
-  .catch(error => {
-    res.status(500).json({error: error})
-  })
+    User.create(userObject)
+    .then(user => {
+      res.sendStatus(200)
+    })
+    .catch(error => {
+      res.status(500).json({error: error})
+    })
+  }
 })
 
 router.post("/authenticate", (req, res, next) => {
 
-  let email = req.body.email
-  let queryPassword = req.body.password
+  if(!req.query.siteAuthToken) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  } else if(req.query.siteAuthToken != Config.siteAuthToken) {
+    return res.status(403).json({error: "Unauthorized access, access denied"})
+  } else {
+    let email = req.body.email
+    let queryPassword = req.body.password
 
-  User.getOne({email: email})
-  .then(user => {
-    bcrypt.compare(queryPassword, user.password).then((authResult) => {
-      if(authResult) {
-        let token = jwt.sign({
-          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
-          data: {_id: user._id}
-        }, Config.jwtSecret);
+    User.getOne({email: email})
+    .then(user => {
+      bcrypt.compare(queryPassword, user.password).then((authResult) => {
+        if(authResult) {
+          let token = jwt.sign({
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
+            data: {_id: user._id}
+          }, Config.jwtSecret);
 
-        let response = {
-          token: token,
-          last_authenticated: new Date().getTime()
+          let response = {
+            token: token,
+            last_authenticated: new Date().getTime()
+          }
+
+          res.json(response)
+        } else {
+          res.sendStatus(401)
         }
-
-        res.json(response)
-      } else {
-        res.sendStatus(401)
-      }
-    });
+      });
+    }
   })
   .catch(error => {
     res.status(500).json({error: error})
