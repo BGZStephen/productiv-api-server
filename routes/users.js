ce60679e1fb75e1d1b1810eaaf71de63d4e040b5
Stephen Wright <sjw948@gmail.com>
2017-07-11 21:01:51 +0100

    added user deletion route that is accessible to admin users only

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,70 @@ const User = require('../models/user')
 
 module.exports = router
 
+router.delete("/:userId", (req, res, next) => {
+  if(!req.get('Authorization')) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  } else if(req.get('Authorization') != Config.adminAuthToken) {
+    return res.status(401).json({error: "Unauthorized access, access denied"})
+  } else {
+    User.delete(req.params.userId)
+    .then(() => {
+      res.sendStatus(200)
+    })
+    .catch(error => {
+      res.status(500).json({error: error})
+    })
+  }
+})
+
+router.get("", (req, res, next) => {
+  if(!req.get('Authorization')) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  } else if(req.get('Authorization') != Config.adminAuthToken) {
+    return res.status(401).json({error: "Unauthorized access, access denied"})
+  } else {
+    User.getAll()
+    .then(users => {
+      res.json(users)
+    })
+    .catch(error => {
+      res.status(500).json({error: error})
+    })
+  }
+})
+
+router.get("/:userId", (req, res, next) => {
+
+  if(!req.get('Authorization')) {
+    return res.status(401).json({error: "Authorisation token not supplied"})
+  } else if(req.get('Authorization') != Config.siteAuthToken) {
+    return res.status(401).json({error: "Unauthorized access, access denied"})
+  } else {
+
+    let token = req.get('Token')
+    let decoded = jwt.verify(token, Config.jwtSecret)
+    let userId = decoded.data._id
+
+    // check to ensure user is only able to access their own user profile, even if other id is entered as query param
+    if(userId != req.params.userId) {
+      return res.status(403).json({error: "Unauthorized access, access denied"})
+    } else {
+      User.getOne({_id: userId})
+      .then(user => {
+        res.json({
+          _id: user._id,
+          email: user.email,
+          first_name: user.first_name,
+          last_name: user.last_name,
+        })
+      })
+      .catch(error => {
+        res.status(500).json({error: error})
+      })
+    }
+  }
+})
+
 router.post("", (req, res, next) => {
 
   if(!req.query.siteAuthToken) {
@@ -61,54 +125,6 @@ router.post("/authenticate", (req, res, next) => {
           res.sendStatus(401)
         }
       })
-    })
-    .catch(error => {
-      res.status(500).json({error: error})
-    })
-  }
-})
-
-router.get("/:userId", (req, res, next) => {
-
-  if(!req.get('Authorization')) {
-    return res.status(401).json({error: "Authorisation token not supplied"})
-  } else if(req.get('Authorization') != Config.siteAuthToken) {
-    return res.status(401).json({error: "Unauthorized access, access denied"})
-  } else {
-
-    let token = req.get('Token')
-    let decoded = jwt.verify(token, Config.jwtSecret)
-    let userId = decoded.data._id
-
-    // check to ensure user is only able to access their own user profile, even if other id is entered as query param
-    if(userId != req.params.userId) {
-      return res.status(403).json({error: "Unauthorized access, access denied"})
-    } else {
-      User.getOne({_id: userId})
-      .then(user => {
-        res.json({
-          _id: user._id,
-          email: user.email,
-          first_name: user.first_name,
-          last_name: user.last_name,
-        })
-      })
-      .catch(error => {
-        res.status(500).json({error: error})
-      })
-    }
-  }
-})
-
-router.get("", (req, res, next) => {
-  if(!req.get('Authorization')) {
-    return res.status(401).json({error: "Authorisation token not supplied"})
-  } else if(req.get('Authorization') != Config.adminAuthToken) {
-    return res.status(401).json({error: "Unauthorized access, access denied"})
-  } else {
-    User.getAll()
-    .then(users => {
-      res.json(users)
     })
     .catch(error => {
       res.status(500).json({error: error})
