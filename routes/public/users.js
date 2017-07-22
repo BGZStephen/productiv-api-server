d420e4350ade6f4192e66af1dc7d42db02c479be
Stephen Wright <sjw948@gmail.com>
2017-07-22 13:18:01 +0100

    removed User functions from models.js and integrated with route funcs

--- routes/public/users.js
+++ routes/public/users.js
@@ -1,6 +1,7 @@
 const Config = require('../../config');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+// const mongoose = require('mongoose');
 const User = require('../../models/user');
 const Auth = require('../../helpers/auth')
 
@@ -31,21 +32,20 @@ module.exports.getUser = function(req, res) {
 
 	// check to ensure user is only able to access their own user profile, even if other id is entered as query param
 	if(userId != req.params.userId) {
-		return res.status(403).json({error: 'Unauthorized access, access denied'});
-	} else {
-		User.getOne({_id: userId})
-		.then(user => {
-			res.json({
-				_id: user._id,
-				email: user.email,
-				firstName: user.firstName,
-				lastName: user.lastName,
-			});
-		})
-		.catch(error => {
-			res.status(500).send(error);
-		});
+		return res.status(403).send({error: 'Unauthorized access, access denied'});
 	}
+
+	User.findById(userId)
+	.then(user => {
+		if(user == null) {
+			return res.status(500).json({error: 'User not found'});
+		} else {
+			res.json(user);
+		}
+	})
+	.catch(error => {
+		res.status(500).send(error);
+	});
 };
 
 module.exports.create = function(req, res) {
@@ -61,17 +61,22 @@ module.exports.create = function(req, res) {
 		password: req.body.password
 	});
 
-	User.create(userObject)
+	User.find({email: req.body.email})
 	.then(user => {
-
+		if (user != null) {
+			return res.status(500).json({error: 'Email address already in use'});
+		}
+		return userObject.save(userObject)
+	})
+	.then(user => {
 		let token = jwt.sign({
 			exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
 			data: {_id: user._id}
 		}, Config.jwtSecret);
 
 		let response = {
-			token: token,
-			last_authenticated: new Date().getTime()
+			token,
+			last_authenticated: new Date().getTime(),
 		};
 
 		res.status(200).json(response);
@@ -87,39 +92,36 @@ module.exports.authenticate = function(req, res) {
 		return res.status(401).json({error: 'Unauthorized access, access denied'});
 	}
 
-	let email = req.body.email;
-	let queryPassword = req.body.password;
+	User.findOne({email: req.body.email})
+	.then(user => {
+		if(authorized.route == 'admin' && user.role != 'admin') {
+			return res.status(401).json({error: 'Unauthorized access, access denied'});
+		}
 
-	User.getOne({email: email})
-		.then(user => {
-			if(authorized.route == 'admin' && user.role != 'admin') {
-				return res.status(401).json({error: 'Unauthorized access, access denied'});
-			}
-			bcrypt.compare(queryPassword, user.password)
-				.then((authResult) => {
-					if(authResult) {
-						let token = jwt.sign({
-							exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
-							data: {
-								_id: user._id,
-								role: user.role
-							}
-						}, Config.jwtSecret);
+		if(user == null) {
+			return res.status(401).json({error: 'Incorrect email address or password'});
+		}
 
-						let response = {
-							token: token,
-							last_authenticated: new Date().getTime()
-						};
+		bcrypt.compare(req.body.password, user.password)
+		.then((authResult) => {
+			if(authResult) {
+				let token = jwt.sign({
+					exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
+					data: {_id: user._id, role: user.role}
+				}, Config.jwtSecret);
 
-						res.json(response);
-					} else {
-						res.sendStatus(401);
-					}
-				})
-				.catch(error => {
-					res.status(500).send(error);
+				res.json({
+					token,
+					last_authenticated: new Date().getTime(),
 				});
-		});
+			} else {
+				return res.status(401).json({error: 'Incorrect email address or password'});
+			}
+		})
+	})
+	.catch(error => {
+		res.status(500).send(error);
+	});
 };
 
 module.exports.update = function(req, res) {
@@ -135,21 +137,26 @@ module.exports.update = function(req, res) {
 	if(req.body.type == 'profile') {
 
 		User.update(userId, req.body.updates)
-    .then(res.sendStatus(200))
+    .then(user => {
+			if(user.nModified >= 1) {
+				res.sendStatus(200);
+			} else {
+				return res.status(500).json({error: 'User update failed'});
+			}
+		})
     .catch(error => {
       res.status(500).json({error: error});
     });
 	} else if (req.body.type == 'password') {
 
-		let userObject = {
-			_id: userId,
-			password: req.body.password,
-		};
-
-		User.updatePassword(userObject)
-		.then(res.sendStatus(200))
+		User.findById(userId)
+		.then(user => {
+			user.password = bcrypt.hashSync(req.body.password, 8);
+			user.markModified('password');
+			user.save(res.sendStatus(200));
+		})
 		.catch(error => {
-			res.status(500).send(error);
-		});
+      res.status(500).json({error: error});
+    });
 	}
 };
