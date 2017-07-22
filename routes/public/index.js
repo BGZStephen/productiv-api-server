8ce16b122f7b3d52832a544489c84f1634cbc5ad
Stephen Wright <sjw948@gmail.com>
2017-07-22 18:14:40 +0100

    added async wrapper functions to ensure errors are caught on user funcs

--- routes/public/index.js
+++ routes/public/index.js
@@ -4,11 +4,11 @@ const business = require('./business')
 
 // Users
 router.post('/users', users.createUser);
-router.all('/users/:userId*', users.loadUser)
+router.post('/users/authenticate', users.authenticate);
+router.all('/users/:userId**', users.loadUser)
 router.delete('/users/:userId', users.deleteUser);
 router.get('/users/:userId', users.getUser);
 router.put('/users/:userId', users.updateUser);
-router.post('/users/authenticate', users.authenticate);
 
 // Business
 router.post('/business', business.createBusiness);
--- routes/public/users.js
+++ routes/public/users.js
@@ -9,151 +9,103 @@ module.exports.loadUser = function(req, res, next) {
 
 	User.findById(userId)
 	.then(user => {
-		if(user == null) {
-			return res.status(500).send('User not found')
-		} else {
-			req.user = user
-			next()
-		}
+		if(user == null) return res.status(500).send('User not found')
+		else {req.user = user; next()}
 	})
 };
 
 module.exports.deleteUser = function(req, res) {
 	const authorized = Auth.checkToken(req.get('Authorization'));
-	if(!authorized.success) {
-		return res.status(401).json(authorized.message);
-	}
+	if(!authorized.success) return res.status(401).json(authorized.message);
 
 	User.delete(req.params.userId)
-	.then(() => {
-		res.sendStatus(200);
-	})
-	.catch(error => {
-		res.status(500).send(error);
-	});
+	.then(res.sendStatus(200))
+	.catch(error => res.status(500).send(error));
 };
 
 module.exports.getUser = function(req, res) {
 	const authorized = Auth.checkToken(req.get('Authorization'));
-	if(!authorized.success) {
-		return res.status(401).json(authorized.message);
-	}
+	if(!authorized.success) return res.status(401).json(authorized.message)
 
 	let decodedJwt = jwt.verify(req.get('Token'), Config.jwtSecret);
 	let userId = decodedJwt.data._id;
 
 	// check to ensure user is only able to access their own user profile, even if other id is entered as query param
-	if(userId != req.params.userId) {
-		return res.status(403).send('Unauthorized access, access denied');
-	}
+	if(userId != req.params.userId) return res.status(403).send('Unauthorized access, access denied');
 
 	return res.json(req.user);
 };
 
 module.exports.createUser = function(req, res) {
-	const authorized = Auth.checkToken(req.get('Authorization'));
-	if(!authorized.success) {
-		return res.status(401).json(authorized.message);
+	const processCreateUser = async function() {
+		const authorized = Auth.checkToken(req.get('Authorization'));
+		if(!authorized.success) return res.status(401).json(authorized.message);
+
+		const userExistsCheck = await User.findOne({email: req.body.email})
+		if(userExistsCheck != null) return res.status(500).send('Email address already in use');
+
+		const user = await new User(req.body).save()
+
+		let token = jwt.sign({
+			exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
+			data: {_id: user._id, role: user.role}
+		}, Config.jwtSecret);
+
+		return res.status(200).json({token, last_authenticated: new Date().getTime()});
 	}
 
-	let userObject = new User({
-		email: req.body.email,
-		firstName: req.body.firstName,
-		lastName: req.body.lastName,
-		password: req.body.password
-	});
-
-	const createUser = async function(userObject) {
-		const userExistsCheck = await User.findOne({email: req.body.email})
-		if(userExistsCheck != null) {
-			return res.status(500).send('Email address already in use');
-		}
-
-		userObject.save(userObject)
-		.then(user => {
-			let token = jwt.sign({
-				exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
-				data: {_id: user._id}
-			}, Config.jwtSecret);
-
-			let response = {
-				token,
-				last_authenticated: new Date().getTime(),
-			};
-
-			return res.status(200).json(response);
-		})
-	};
-
-	createUser(userObject)
-	.catch(error => {
-		return res.status(500).send(error);
-	});
-};
+	processCreateUser()
+	.catch(error => res.status(500).send(error));
+}
 
 module.exports.authenticate = function(req, res) {
-	const authorized = Auth.checkToken(req.get('Authorization'));
-	if(!authorized.success) {
-		return res.status(401).json(authorized.message);
-	}
+	const processAuthenticate = async function() {
+		const authorized = await Auth.checkToken(req.get('Authorization'))
+		if(!authorized.success) return res.status(401).json(authorized.message);
 
-	User.findOne({email: req.body.email})
-	.then(user => {
-		if(authorized.route == 'admin' && user.role != 'admin') {
-			return res.status(401).send('Unauthorized access, access denied');
-		}
+		const user = await User.findOne({email: req.body.email})
+		if (authorized.route == 'admin' && user.role != 'admin') return res.status(401).send('Unauthorized access, access denied')
+		if (user == null) return res.status(401).send('Incorrect email address or password');
 
-		if(user == null) {
+		const bcyptCompare = bcrypt.compare(req.body.password, user.password)
+		if(bcyptCompare) {
+			let token = jwt.sign({
+				exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
+				data: {_id: user._id, role: user.role}
+			}, Config.jwtSecret);
+			res.json({
+				token,
+				last_authenticated: new Date().getTime(),
+			});
+		} else {
 			return res.status(401).send('Incorrect email address or password');
 		}
+	}
 
-		bcrypt.compare(req.body.password, user.password)
-		.then((authResult) => {
-			if(authResult) {
-				let token = jwt.sign({
-					exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365),
-					data: {_id: user._id, role: user.role}
-				}, Config.jwtSecret);
-
-				res.json({
-					token,
-					last_authenticated: new Date().getTime(),
-				});
-			} else {
-				return res.status(401).send('Incorrect email address or password');
-			}
-		})
-	})
-	.catch(error => {
-		res.status(500).send(error);
-	});
+	processAuthenticate()
+	.catch(error => res.status(500).send(error));
 };
 
 module.exports.updateUser = function(req, res) {
-	const authorized = Auth.checkToken(req.get('Authorization'));
-	if(!authorized.success) {
-		return res.status(401).json(authorized.message);
+	const processUpdateUser = async function() {
+		const authorized = Auth.checkToken(req.get('Authorization'));
+		if(!authorized.success) return res.status(401).json(authorized.message);
+
+		let decodedJwt = jwt.verify(req.get('Token'), Config.jwtSecret);
+		let userId = decodedJwt.data._id;
+
+		if(req.body.type == 'profile') {
+			const user = await User.update(userId, req.body.updates)
+			if(user.nModified >= 1) return res.sendStatus(200);
+			else return res.status(500).send('User update failed');
+		};
+
+		if (req.body.type == 'password') {
+			req.user.password = bcrypt.hashSync(req.body.password, 8);
+			req.user.markModified('password').save(res.sendStatus(200));
+		};
 	}
 
-	let decodedJwt = jwt.verify(req.get('Token'), Config.jwtSecret);
-	let userId = decodedJwt.data._id;
-
-	if(req.body.type == 'profile') {
-
-		User.update(userId, req.body.updates)
-    .then(user => {
-			if(user.nModified >= 1) {
-				res.sendStatus(200);
-			} else {
-				return res.status(500).send('User update failed');
-			}
-		})
-    .catch(error => {
-      res.status(500).send(error);
-    });
-	} else if (req.body.type == 'password') {
-		req.user.password = bcrypt.hashSync(req.body.password, 8);
-		req.user.markModified('password');
-		req.user.save(res.sendStatus(200));
-	}
+	processUpdateUser()
+	.catch(error => res.status(500).send(error));
 };
