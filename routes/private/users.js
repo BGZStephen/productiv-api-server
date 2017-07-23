7f8a2615da521496b51148590dc9ac867522d45a
Stephen Wright <sjw948@gmail.com>
2017-07-23 11:36:35 +0100

    improvder users getAll function & fixed bug in DB query

--- routes/private/users.js
+++ routes/private/users.js
@@ -4,23 +4,23 @@ const User = require('../../models/user');
 const Auth = require('../../helpers/auth');
 
 module.exports.getAll = function(req, res) {
-	const authorized = Auth.routeAuth(req.get('Authorization'));
-	if(!authorized) {
-		return res.status(401).json({error: 'Unauthorized access, access denied'});
+	const processGetAll = async function() {
+		const authorized = Auth.checkToken(req.get('Authorization'));
+		if(!authorized) return res.status(401).json({error: 'Unauthorized access, access denied'});
+
+		const decodedJwt = jwt.verify(req.get('Token'), Config.jwtSecret);
+		const role = decodedJwt.data.role;
+
+		if(role != 'admin') return res.status(401).json({error: 'Unauthorized access, access denied'});
+
+		const users = await User.find({})
+		if(users.length == 0) {
+			res.status(500).send("No users found")
+		} else {
+			res.json(users)
+		}
 	}
 
-	const decodedJwt = jwt.verify(req.get('Token'), Config.jwtSecret);
-	const role = decodedJwt.data.role;
-
-	if(role != 'admin') {
-		return res.status(401).json({error: 'Unauthorized access, access denied'});
-	}
-
-	User.getAll()
-	.then(users => {
-		res.json(users);
-	})
-	.catch(error => {
-		res.status(500).send(error);
-	});
+	processGetAll()
+	.catch(error => res.status(500).send(error));
 };
